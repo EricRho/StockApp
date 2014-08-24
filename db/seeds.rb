76527fe96a5e5874c9e48db737f05e04e1845d33
EricRho <eric.rho.87@gmail.com>
2014-08-24 19:13:09 -0400

    No significant changes. Prepare to create new branch to impliment new features

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 Stock.delete_all
 
 
-Stock.create!(symbol: 'MNKD', name: 'Mannkind Corporat', bid: 7.17, ask: 7.28, year_low: 3.80, year_heigh: 11.48)
+Stock.create!(symbol: 'MNKD', name: 'Mannkind Corporat')
 Stock.create!(symbol: 'ARRY', name: 'Array BioPharma Inc.')
 Stock.create!(symbol: 'SNY', name: 'Sanofi')
 Stock.create!(symbol: 'LLY', name: 'Eli Lilly and Company')
