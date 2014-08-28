3daee2e13f53f9944da1567cea32463a67387283
EricRho <eric.rho.87@gmail.com>
2014-08-28 19:42:42 -0400

    Roll back. Start over point.

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20140826143947) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "derivatives", force: true do |t|
     t.string   "symbol"
     t.string   "name"
@@ -58,7 +55,7 @@ ActiveRecord::Schema.define(version: 20140826143947) do
     t.string   "iud"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
